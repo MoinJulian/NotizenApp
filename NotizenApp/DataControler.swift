e926e4aeb968b9fbcf9088dc9f08d08c76e5958a
Julian Hammer <jh@moinjulian.com>
2024-06-21 09:38:00 +0100

    Init CoreData

---
+++ NotizenApp/DataControler.swift
@@ -0,0 +1,21 @@
+//
+//  DataControler.swift
+//  NotizenApp
+//
+//  Created by Julian on 21/06/2024.
+//
+
+import Foundation
+import CoreData
+
+class DataController: ObservableObject {
+    var container: NSPersistentContainer
+    init(name: String) {
+    container = NSPersistentContainer(name: name)
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                print("CoreData Error: \(error.localizedDescription)")
+            }
+        }
+    }
+}
--- NotizenApp/NotizenAppApp.swift
+++ NotizenApp/NotizenAppApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct NotizenAppApp: App {
+    
+    @StateObject private var dataController = DataController(name: "Model")
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
         }
     }
 }
